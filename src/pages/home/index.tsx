9207d8a9cbd29fd4faaa31354a53d94440c6a58d
isisnaldojoao <isisnaldojoao@gmail.com>
2023-12-26 19:24:06 -0300

    finallll version

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -76,7 +76,7 @@ export function Home() {
     return (
         <div className="flex flex-col w-full py-4 items-center justify-center">
             <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
-                João Isisnaldo
+                João & Talita
             </h1>
             <span className="text-gray-50 mb-5 mt-3">
                 Veja meus links 👇
